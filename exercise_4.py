1f1e8a049328c6fb00c2277f44c4ff596248d81c
ÍIAndreasII <andreas.bartilson@gmail.com>
2023-04-11 10:27:24 +0200

    Exercises 4 & 5

---
+++ exercise_4.py
@@ -0,0 +1,127 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def lagrange_interpolation(x, y):
+    n = len(x)
+    def L(k, x_i):
+        out = 1.0
+        for i in range(n):
+            if i != k:
+                out *= (x_i - x[i])/(x[k] - x[i])
+        return out
+    def P(x_i):
+        out = 0.0
+        for k in range(n):
+            out += y[k]*L(k, x_i)
+        return out
+    return P
+
+# Define the function to interpolate
+def f(x):
+    return np.exp(-x**2)
+
+# Define the interval
+a, b = -1.0, 1.0
+
+# Define the values of N to use
+N_values = [5, 10, 20, 40]
+
+def show_lagrange():
+    # Plot the results for each value of N
+    for N in N_values:
+        # Define the equidistant spaced nodes
+        x = np.linspace(a, b, N+1)
+        y = f(x)
+
+        # Compute the Lagrange polynomial
+        P = lagrange_interpolation(x, y)
+
+        # Evaluate the Lagrange polynomial at some points
+        x_vals = np.linspace(a, b, 100)
+        y_vals = P(x_vals)
+
+        # Plot the results
+        plt.plot(x_vals, y_vals, label=f'N={N}')
+
+    # Plot the true function
+    x_vals = np.linspace(a, b, 100)
+    f_vals = f(x_vals)
+    plt.plot(x_vals, f_vals, '--', label='True Function')
+
+    plt.legend()
+    plt.show()
+
+def chebyshev_roots(N):
+    """
+    Compute the roots of the Chebyshev polynomial of degree N
+    """
+    n = np.arange(1, N+1)
+    x = np.cos((2*n-1)*np.pi/(2*N))
+    return x
+
+def show_chebyshev():
+    # Define the degree of the Chebyshev polynomial
+    N = 10
+
+    # Compute the Chebyshev nodes
+    x = chebyshev_roots(N+1)
+    x = 0.5*(a + b + (b - a)*x)
+
+    # Evaluate f at the Chebyshev nodes
+    y = f(x)
+
+    # Compute the Lagrange polynomial
+    P = lagrange_interpolation(x, y)
+
+    # Evaluate the Lagrange polynomial at some points
+    x_vals = np.linspace(a, b, 100)
+    y_vals = P(x_vals)
+
+    # Plot the results
+    plt.plot(x_vals, y_vals, label='P_chev')
+    plt.plot(x, y, 'o', label='Nodes')
+    #plt.plot(x_vals, f(x_vals), label='True Function')
+    plt.legend()
+    plt.show()
+
+
+# Define the values of N to use
+N_values = [4, 7, 10]
+
+def show_err():
+    # Define the x values at which to compute the errors
+    x_vals = np.linspace(a, b, 100)
+
+    # Compute the equidistant spaced nodes
+    for N in N_values:
+        x = np.linspace(a, b, N+1)
+        y = f(x)
+
+        # Compute the Lagrange polynomial
+        P_eq = lagrange_interpolation(x, y)
+
+        # Compute the absolute errors
+        err_eq = np.abs(f(x_vals) - P_eq(x_vals))
+
+        # Plot the results
+        plt.plot(x_vals, err_eq, label=f'N={N}')
+
+    # Compute the Chebyshev nodes
+    for N in N_values:
+        x = chebyshev_roots(N+1)
+        x = 0.5*(a + b + (b - a)*x)
+        y = f(x)
+
+        # Compute the Lagrange polynomial
+        P_cheb = lagrange_interpolation(x, y)
+
+        # Compute the absolute errors
+        err_cheb = np.abs(f(x_vals) - P_cheb(x_vals))
+
+        # Plot the results
+        plt.plot(x_vals, err_cheb, '--', label=f'N={N}')
+
+    plt.legend()
+    plt.show()
+
+show_chebyshev()
---
+++ exercise_5.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+A = np.array([[1, 0, 1], [1, 0, 1], [1, 1, 0], [1, 1, 0], [1, 1, 2]])
+b = np.array([3, 2, 3, 4, 6])
+
+Q, R = np.linalg.qr(A)
+x = np.linalg.solve(R, Q.T @ b)
+
+x_vals = np.linspace(-1, 2, 50)
+y_vals = np.linspace(-1, 3, 50)
+x_mesh, y_mesh = np.meshgrid(x_vals, y_vals)
+
+z_mesh = x[0] + x[1]*x_mesh + x[2]*y_mesh
+
+c0 = x[0]
+c1 = x[1]
+c2 = x[2]
+
+print(f"z = {x[0]:.4f} + {x[1]:.4f}x + {x[2]:.4f}y")
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(A[:, 1], A[:, 2], b, c='red', marker='o', label='Data points')
+ax.plot_surface(x_mesh, y_mesh, z_mesh, alpha=0.5)
+
+ax.set_xlabel('X axis')
+ax.set_ylabel('Y axis')
+ax.set_zlabel('Z axis')
+
+ax.legend()
+
+plt.show()
